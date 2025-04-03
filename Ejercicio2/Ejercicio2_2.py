097e00236c30a673b9244db6035761d2824b19b7
Aarón Sacristán <aaronsacristan6a@gmail.com>
2025-04-03 19:46:34 +0200

    Cambios6

--- Ejercicio2/Ejercicio2_2.py
+++ Ejercicio2/Ejercicio2_2.py
@@ -1,6 +1,8 @@
 def determinante( matriz):
 
-    
+    if len(matriz) == 2:
+        return matriz[0][0] * matriz[1][1] - matriz[0][1] * matriz[1][0]
+
     det = 0
     for columna in range(len(matriz[0])):
         matriz2 = [fila[:columna] + fila[columna + 1:] for fila in matriz[1:]]
---
+++ Ejercicio3/Ejercicio3.py
@@ -0,0 +1,13 @@
+naves = [
+    {"nombre": "Cometa Veloz", "longitud": 150, "tripulantes": 10, "pasajeros": 100},
+    {"nombre": "Titán del Cosmos", "longitud": 200, "tripulantes": 20, "pasajeros": 150},
+    {"nombre": "nave1", "longitud": 180, "tripulantes": 15, "pasajeros": 120},
+    {"nombre": "nave2", "longitud": 250, "tripulantes": 30, "pasajeros": 200},
+    {"nombre": "nave3", "longitud": 170, "tripulantes": 12, "pasajeros": 100},
+    {"nombre": "nave4", "longitud": 160, "tripulantes": 18, "pasajeros": 120},
+    {"nombre": "nave5", "longitud": 220, "tripulantes": 25, "pasajeros": 180},
+    {"nombre": "nave6", "longitud": 140, "tripulantes": 8, "pasajeros": 60},
+    {"nombre": "nave7", "longitud": 160, "tripulantes": 14, "pasajeros": 90},
+    {"nombre": "nave8", "longitud": 190, "tripulantes": 22, "pasajeros": 110}
+]
+
