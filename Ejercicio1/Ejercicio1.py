04667d6a32c6e00170d126894d374fbeee9d221f
Aarón Sacristán <aaronsacristan6a@gmail.com>
2025-04-03 18:20:16 +0200

    Ejercicio1

--- Ejercicio1/Ejercicio1.py
+++ Ejercicio1/Ejercicio1.py
@@ -3,10 +3,15 @@ def piramide (piedras, col1, col2, col3):
     
     def mover(piedras, col1, col2, col3):
         if piedras == 1:
-            movimientos.append()
+            movimientos.append(piedras)
         else:
             mover(piedras - 1, col1, col3, col2)
             movimientos.append((col1, col2))
             mover(piedras - 1, col3, col2, col1)
         
     mover(piedras, col1, col2, col3)
+    return movimientos
+
+movimientos = piramide( 20, "columna1", "columna2", "columna3")
+for i in movimientos: 
+    print(i)
