664aeb97ad69c97fe75d3adbe91fe4859ca59f3c
Aarón Sacristán <aaronsacristan6a@gmail.com>
2025-04-03 18:16:03 +0200

    Cambios2

--- Ejercicio1/Ejercicio1.py
+++ Ejercicio1/Ejercicio1.py
@@ -1,2 +1,12 @@
 def piramide (piedras, col1, col2, col3):
     movimientos = []
+    
+    def mover(piedras, col1, col2, col3):
+        if piedras == 1:
+            movimientos.append()
+        else:
+            mover(piedras - 1, col1, col3, col2)
+            movimientos.append((col1, col2))
+            mover(piedras - 1, col3, col2, col1)
+        
+    mover(piedras, col1, col2, col3)
